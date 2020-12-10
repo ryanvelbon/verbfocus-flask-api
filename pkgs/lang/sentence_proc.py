088711b9164ba348289af4fb6aa4eae04d89912e
Ryan <vellabonelloryan@gmail.com>
2020-12-10 08:02:57 +0300

    Now in cmd from inside base dir you can run tests 'python -m tests.test_moduleB'

---
+++ pkgs/lang/sentence_proc.py
@@ -0,0 +1,13 @@
+# functions for processing sentences
+
+def correct_casing(s):
+	return s
+
+def correct_punctuation(s):
+	return s
+
+# simply returns the number of lines
+# add more functionality by invoking functions which verify the validity of a sentence
+def sentences_count(txtfile):
+	return len(txtfile.readlines())
+
---
+++ pkgs/pkg/factus_est.py
@@ -0,0 +1,7 @@
+# this is an example of how packages, modules and functions should be orgranized in the filesystem
+
+# mypackage/mymodule.py
+
+
+def myfunction(name):
+	print("Hello, " + name)
